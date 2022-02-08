838f40e6437e6928375f4698b54b83e5c4c103d9
Meet Madhu <meet.madhu@MBProC-MeetM.hitronhub.home>
2022-02-08 14:55:50 -0800

    initial commit

---
+++ src/components/AddUsers/AddNewUser.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const AddNewUser = () => {};
+
+export default AddNewUser;
---
+++ src/components/UserList/UserList.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const UserList = () => {};
+
+export default UserList;
